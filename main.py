1bcc2b523641ead135132030510b433dcb414afa
Mônica Iara de Araújo <monicaiaraujo@hotmail.com>
2026-06-23 14:37:39 -0300

    Update main.py
    feat: Dados de contato obtidos com sucesso do Subase

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Ambiente configurado com sucesso!")
+import os
+from dotenv import load_dotenv
+from supabase import create_client
+
+# 1. Carrega as chaves no arquivo .env
+load_dotenv()
+
+URL_DO_SUPABASE = os.getenv("SUPABASE_URL")
+CHAVE_DO_SUPABASE = os.getenv("SUPABASE_KEY")
+
+# 2. Conecta com o site do Supabase as chaves
+supabase = create_client(URL_DO_SUPABASE, CHAVE_DO_SUPABASE)
+
+print("Conectando ao banco de dados...")
+
+# 3. Faz a busca na tabela de contatos
+resposta = supabase.table("contatos").select("*").execute()
+
+# 4. Mostra na tela os dados 
+print("\n--- CONTATOS ENCONTRADOS NO BANCO ---")
+print(resposta.data)
